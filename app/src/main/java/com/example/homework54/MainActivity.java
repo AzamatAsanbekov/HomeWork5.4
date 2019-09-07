29ac87b50216e00e9e059e90b32720b4f1123e05
AzamatAsanbekov <azamat.asanbekov18@gmail.com>
2019-09-07 18:45:03 +0600

    Initial commit

---
+++ app/src/main/java/com/example/homework54/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.homework54;
+
+import androidx.annotation.Nullable;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    TaskAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.task_list);
+
+        adapter = new TaskAdapter();
+        recyclerView.setAdapter(adapter);
+
+        RecyclerView.LayoutManager manager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(manager);
+
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, TaskDelailsActivity.class);
+        startActivityForResult(intent, 100);
+
+    }
+
+    private void openDetails(Task task) {
+        Intent intent = new Intent(MainActivity.this, TaskDelailsActivity.class);
+        intent.putExtra(TaskDelailsActivity.DETAILS_KEY, task);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            if (resultCode == RESULT_OK) {
+                Task task = (Task) data.getSerializableExtra("key");
+                adapter.addTask(task);
+
+            }
+        }
+
+
+    }
+}
